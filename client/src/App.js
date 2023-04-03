18eb8a617ad18b30090f3081ab11690c0d16b545
marcelkawski <marcel.kawski1@gmail.com>
2023-04-03 16:32:50 +0300

    Separate docker-compose.yml files for development and production

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Fibonacci Calculator</h1>
+          <h1 className="App-title">Fibonacci Calculator 🖩</h1>
           <Link to="/otherpage">Other Page</Link>
           <div>
             <Route exact path="/" component={Fibonacci} />
